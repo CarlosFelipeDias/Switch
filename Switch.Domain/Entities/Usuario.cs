e1924beda3309d4b5b77ca488d33c60cab31e276
carlosfelipe <carlosfelipedias@gmail.com>
2020-01-30 23:41:13 -0300

    Criar consoleApp para realizar testes envolvendo o Entity.

--- Switch.Domain/Entities/Usuario.cs
+++ Switch.Domain/Entities/Usuario.cs
@@ -15,25 +15,36 @@ namespace Switch.Domain.Entities
             Amigos = new List<Amigo>();
         }
 
-        public int Id { get;private set; }
+        public Usuario(string nome, string sobreNome, string email, string senha, DateTime dataNascimento, SexoEnum sexo, string urlFoto)
+        {
+            Nome = nome;
+            SobreNome = sobreNome;
+            Email = email;
+            Senha = senha;
+            DataNascimento = dataNascimento;
+            Sexo = sexo;
+            UrlFoto = urlFoto;
+        }
+
+        public int Id { get; private set; }
         public string Nome { get; private set; }
         public string SobreNome { get; private set; }
         public string Email { get; private set; }
         public string Senha { get; private set; }
-        public DateTime  DataNascimento { get; private set; }
+        public DateTime DataNascimento { get; private set; }
         public SexoEnum Sexo { get; private set; }
         public string UrlFoto { get; private set; }
 
-        public virtual Identificacao Identificacao { get;  set; }
+        public virtual Identificacao Identificacao { get; set; }
         public virtual StatusRelacionamento StatusRelacionamento { get; set; }
         public virtual ProcurandoPor ProcurandoPor { get; set; }
 
 
-        public virtual ICollection<Postagem> Postagens { get;  set; }
-        public virtual ICollection<UsuarioGrupo> UsuarioGrupos { get;  set; }
-        public virtual ICollection<LocalTrabalho> LocaisTrabalho { get;  set; }
-        public virtual ICollection<InstituicaoEnsino> InstituicoesEnsino { get;  set; }
-        public virtual ICollection<Amigo> Amigos { get;  set; }
+        public virtual ICollection<Postagem> Postagens { get; set; }
+        public virtual ICollection<UsuarioGrupo> UsuarioGrupos { get; set; }
+        public virtual ICollection<LocalTrabalho> LocaisTrabalho { get; set; }
+        public virtual ICollection<InstituicaoEnsino> InstituicoesEnsino { get; set; }
+        public virtual ICollection<Amigo> Amigos { get; set; }
         public virtual ICollection<Comentario> Comentarios { get; set; }
 
 
---
+++ SwitchAPP/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+using Switch.Infra.Data.Context;
+using System;
+
+namespace SwitchAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuario = new Usuario("Usiário", "teste", "userTeste@gmail.com", "123", DateTime.Now, Switch.Domain.Enuns.SexoEnum.Masculino, @"C:\temp");
+
+            var optionsBuilder = new DbContextOptionsBuilder<SwitchContext>();
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseMySql("Server=localhost;userid=root;password=1234;database=SwitchDB;", m => m.MigrationsAssembly("Switch.Infra.Data").MaxBatchSize(1000));
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                using (var dbcontext = new SwitchContext(optionsBuilder.Options))
+                {
+                    dbcontext.Usuarios.Add(usuario);
+                    dbcontext.SaveChanges();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Ok!");
+            Console.ReadKey();
+        }
+
+    }
+}
+
